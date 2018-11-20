cd0dd207d5a1640c4dfa234a69c8bff18dcef4fd
Son Minh Dao <dev@onewits.com>
2018-11-19 21:16:52 -0600

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AppRoutingModule } from './app-routing.module';
 // External Libs
 import { PrimeNgModule } from './`external-libs/prime-ng.module';
 import { CoreUIModule } from './`external-libs/core-ui.module';
+import { NgMaterialModule } from './`external-libs/material.module';
 
 // Samples
 import { CoreUISamplesModule } from './samples/core-ui/core-ui.module';
@@ -43,6 +44,7 @@ import { CoreUISamplesModule } from './samples/core-ui/core-ui.module';
     // External Libs
     CoreUIModule,
     PrimeNgModule,
+    NgMaterialModule,
     // Samples
     CoreUISamplesModule
   ],
