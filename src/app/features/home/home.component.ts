ab3a11dbb052807c06045c3b4495528f091186b5
Son Minh Dao <dev@onewits.com>
2018-11-19 20:48:23 -0600

    impl fw

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
-import { getStyle, rgbToHex } from '@coreui/coreui/dist/js/coreui-utilities';
 
 import { QuoteService } from './quote.service';
 
@@ -13,51 +12,19 @@ export class HomeComponent implements OnInit {
   quote: string;
   isLoading: boolean;
 
-  public themeColors(): void {
-    Array.from(document.querySelectorAll('.theme-color')).forEach(function(el) {
-      const elem = document.getElementsByClassName(el.classList[0])[0];
-      const background = getStyle('background-color', elem);
+  constructor(private quoteService: QuoteService) {}
 
-      const table = document.createElement('table');
-      table.innerHTML = `
-        <table class="w-100">
-          <tr>
-            <td class="text-muted">HEX:</td>
-            <td class="font-weight-bold">${rgbToHex(background)}</td>
-          </tr>
-          <tr>
-            <td class="text-muted">RGB:</td>
-            <td class="font-weight-bold">${background}</td>
-          </tr>
-        </table>
-      `;
-
-      el.parentNode.appendChild(table);
-    });
-  }
-
-  ngOnInit(): void {
-    this.themeColors();
+  ngOnInit() {
+    this.isLoading = true;
+    this.quoteService
+      .getRandomQuote({ category: 'dev' })
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe((quote: string) => {
+        this.quote = quote;
+      });
   }
 }
-
-// export class HomeComponent implements OnInit {
-//   quote: string;
-//   isLoading: boolean;
-
-//   constructor(private quoteService: QuoteService) {}
-
-//   ngOnInit() {
-//     this.isLoading = true;
-//     this.quoteService
-//       .getRandomQuote({ category: 'dev' })
-//       .pipe(
-//         finalize(() => {
-//           this.isLoading = false;
-//         })
-//       )
-//       .subscribe((quote: string) => {
-//         this.quote = quote;
-//       });
-//   }
-// }
